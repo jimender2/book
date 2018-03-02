a0c147985b7a0e2b24a95bec323605e4573e26c8
BuildTools <jmeredit@ashland.edu>
2018-03-02 06:37:32 -0500

    Initial commit

---
+++ src/welcome.c
@@ -0,0 +1,20 @@
+/*
+ * welcome.c
+ *
+ *  Created on: Mar 1, 2018
+ *      Author: Jonathan Meredith
+ */
+
+
+/* Fig. 2.1: fig02_01.c
+	A first program in C */
+#include <stdio.h>
+
+/* function main begins program execution */
+int main()
+{
+	printf( "\aWelcome to C!\n" );
+
+	return 0;
+
+}
