fdeb372da7452e05fd58c46bed2e3d0d1e937fd9
BuildTools <jmeredit@ashland.edu>
2018-03-05 19:24:34 -0500

    3.13

---
+++ Chapter 3/3.13.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int c;
+
+	c = 5;
+	printf( "%d\n", c );
+	printf( "%d\n", c++ );
+	printf( "%d\n", c );
+
+	c = 5;
+	printf( "%d\n", c );
+	printf( "%d\n", ++c );
+	printf( "%d\n", c );
+
+	return 0;
+
+}
